67f552055d5d3468fa9ba4b9793f3eb7c18254bc
leresidue <35150107+leresidue@users.noreply.github.com>
2018-01-09 18:49:39 -0500

    Add files via upload

--- window1.cpp
+++ window1.cpp
@@ -2,6 +2,30 @@
 	© 2018 Frédérique Brisson-Lambert
 */
 
+/***************************************************************************************
+
+	Copyright (c) 2018 Frédérique Brisson-Lambert
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+***************************************************************************************/
+
 
 
 #include "stdafx.h"
@@ -11,6 +35,7 @@ extern HINSTANCE	gI;
 
 
 ATOM	Cwindow::CwA = 0;
+INT		Cwindow::CwC = 0;
 const wchar_t	*Cwindow::CnA = L"Cwindow";
 
 LRESULT	Cwindow::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -18,13 +43,16 @@ LRESULT	Cwindow::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	if(message == WM_NCCREATE) {
 		pW = (Cwindow*)((CREATESTRUCT*)lParam)->lpCreateParams;
 		if(pW) pW->oW = hWnd;
+		CwC++;
 		SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)pW);
 	} else if(message == WM_NCDESTROY) {
+		CwC--;
 		pW = (Cwindow*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
 		if(pW) {
 			SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)0);
 			delete pW;
 		}
+		if(CwC == 0) PostQuitMessage(0);
 	} else {
 		pW = (Cwindow*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
 		if(pW) {
